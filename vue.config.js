0f8fb5831e7c7fd4d89aca12ddedef2159f38cb7
Jesse Khorasanee <jessekhorasanee@gmail.com>
2025-06-08 07:05:38 +0800

    set up for heroku

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   chainWebpack: config => {
     // Handle images in assets/images and subfolders
     config.module
